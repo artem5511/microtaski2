ae1a2225ce15e63128244c49769b04b857160181
artem_ <artem_5511@mail.ru>
2023-04-03 00:05:35 +0300

    add callBack

--- src/App.tsx
+++ src/App.tsx
@@ -6,34 +6,40 @@ import {Footer} from './site/Footer';
 import {NewComponent} from './NewComponent';
 import Button from './site/Button';
 
-const students = [
-    {id: 1, name: "James", age: 8},
-    {id: 2, name: "Robert", age: 18},
-    {id: 3, name: "John", age: 28},
-    {id: 3, name: "John", age: 28},
-    {id: 3, name: "John", age: 28},
-    {id: 3, name: "John", age: 28},
-    {id: 4, name: "Michael", age: 38},
-    {id: 4, name: "Michael", age: 38},
-    {id: 5, name: "William", age: 48},
-    {id: 5, name: "William", age: 48},
-    {id: 6, name: "David", age: 58},
-    {id: 7, name: "Richard", age: 68},
-    {id: 7, name: "Richard", age: 68},
-    {id: 7, name: "Richard", age: 68},
-    {id: 8, name: "Joseph", age: 78},
-    {id: 8, name: "Joseph", age: 78},
-    {id: 9, name: "Thomas", age: 88},
-    {id: 10, name: "Charles", age: 98},
-    {id: 11, name: "Christopher", age: 100}
-]
-const topCars = [
-    {manufacturer: "BMW", model: 'm5cs'},
-    {manufacturer: "Mercedes", model: 'e63s'},
-    {manufacturer: "Audi", model: 'rs6'}
-]
+// const students = [
+//     {id: 1, name: "James", age: 8},
+//     {id: 2, name: "Robert", age: 18},
+//     {id: 3, name: "John", age: 28},
+//     {id: 3, name: "John", age: 28},
+//     {id: 3, name: "John", age: 28},
+//     {id: 3, name: "John", age: 28},
+//     {id: 4, name: "Michael", age: 38},
+//     {id: 4, name: "Michael", age: 38},
+//     {id: 5, name: "William", age: 48},
+//     {id: 5, name: "William", age: 48},
+//     {id: 6, name: "David", age: 58},
+//     {id: 7, name: "Richard", age: 68},
+//     {id: 7, name: "Richard", age: 68},
+//     {id: 7, name: "Richard", age: 68},
+//     {id: 8, name: "Joseph", age: 78},
+//     {id: 8, name: "Joseph", age: 78},
+//     {id: 9, name: "Thomas", age: 88},
+//     {id: 10, name: "Charles", age: 98},
+//     {id: 11, name: "Christopher", age: 100}
+// ]
+// const topCars = [
+//     {manufacturer: "BMW", model: 'm5cs'},
+//     {manufacturer: "Mercedes", model: 'e63s'},
+//     {manufacturer: "Audi", model: 'rs6'}
+// ]
 
 function App() {
+    const Button1Foo = () => {
+        console.log('Im Vasya')
+    }
+    const Button2Foo = () => {
+        console.log('Im Ivan')
+    }
     return (
         <div>
             {/*<Header title={'New body'}/>*/}
@@ -41,7 +47,8 @@ function App() {
             {/*<Footer titleForFooter={'NEW FOOTER'} num={5}/>*/}
             {/*<NewComponent students={students}/>*/}
             {/*<NewComponent topCars={topCars}/>*/}
-            <Button/>
+            <Button name={'MyYouTubeChanel-1'} callBack={Button1Foo}/>
+            <Button name={'MyYouTubeChanel-2'} callBack={Button2Foo}/>
         </div>
     )
 }
--- src/site/Button.tsx
+++ src/site/Button.tsx
@@ -1,38 +1,43 @@
 import React from 'react';
-import {logDOM} from '@testing-library/react';
 
-const Button = () => {
+type ButtonPropsType = {
+    name: string
+    callBack: () => void
+}
+
+const Button = (props: ButtonPropsType) => {
     // const myFirstSubscriber = (event: React.MouseEvent<HTMLButtonElement>) => {
     //     console.log("Hello Im Vasya!")
     // }
     // const mySecondSubscriber = (event: React.MouseEvent<HTMLButtonElement>) => {
     //     console.log("Hello Im Ivan!")
     // }
-
     // const onClickHandler = (name: string) => {
     //     console.log(name)
     // }
-
-    const foo1 = (event: React.MouseEvent<HTMLButtonElement>) => {
-        //ничего не передавать, в консоли должно показаться 100200
-        console.log('100200')
+    // const foo1 = () => {
+    //     //ничего не передавать, в консоли должно показаться 100200
+    //     console.log('100200')
+    // }
+    // const foo2 = (num: number) => {
+    //     //передать цифру 100200
+    //     console.log(num)
+    // }
+    const onClickHandler = () => {
+        props.callBack()
     }
-    const foo2 = (num: number) => {
-        //передать цифру 100200
-        console.log(num)
-    }
-
     return (
         <div>
             {/*<button onClick={(event)=>{console.log("Hello!")}}>MyYouTubeChanel-1</button>*/}
+            {/*<button onClick={(event)=>{console.log("Hello!")}}>MyYouTubeChanel-2</button>*/}
             {/*<button onClick={mySecondSubscriber}>MyYouTubeChanel-2</button>*/}
-
             {/*<button onClick={(event)=>{onClickHandler('Vasya')}}>MyYouTubeChanel-3</button>*/}
             {/*<button onClick={(event)=>{onClickHandler('Ivan')}}>MyYouTubeChanel-4</button>*/}
             {/*<button onClick={()=>{onClickHandler('some info')}}>MyYouTubeChanel-5</button>*/}
+            {/*<button onClick={foo1}>1</button>*/}
+            {/*<button onClick={()=>foo2(100200)}>2</button>*/}
 
-            <button onClick={foo1}>1</button>
-            <button onClick={()=>{foo2(100200)}}>2</button>
+            <button onClick={onClickHandler}>{props.name}</button>
         </div>
     );
 };
