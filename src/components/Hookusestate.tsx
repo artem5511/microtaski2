99f51ab6b64068a6f755e064cd909b5c93e0c229
artem_ <artem_5511@mail.ru>
2023-04-03 00:49:37 +0300

    useState finish

--- src/components/Hookusestate.tsx
+++ src/components/Hookusestate.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Hookusestate = () => {
-    let a = 1
+    // let a = 1
+    let [a, setA] = useState(1)
 
     const onClickhandler = () => {
-        ++a;
+        setA(++a);
+        console.log(a)
+    }
+    const onClickhandler2 = () => {
+        setA(0);
         console.log(a)
     }
 
     return (
         <div>
-            <h1>a</h1>
+            <h1>{a}</h1>
             <button onClick={onClickhandler}>number</button>
+            <button onClick={onClickhandler2}>0</button>
         </div>
     );
 };
